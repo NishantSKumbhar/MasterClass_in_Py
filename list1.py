e3df19586cf05d3ce2e9246412923822f9b10707
Nishant <73151518+nishant-coder-ai@users.noreply.github.com>
2021-10-31 08:12:34 +0530

    Add files via upload

---
+++ list1.py
@@ -0,0 +1,22 @@
+"""
+@author : Nishant Sanjay Kumbhar
+@goal   : To implement list data type
+"""
+
+def addition_of_num(num1 , num2):
+    """
+    @num1 : accept the integer
+    @num2 : accept the integer
+    """
+    add = num1 + num2
+    return add 
+
+def substraction_of_num(num1 , num2):
+    
+    sub = num1 - num2
+    return sub 
+
+print(addition_of_num(6, 8))
+print(substraction_of_num(10, 8))
+
+
