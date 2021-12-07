800e8496a256141e1a26343d0d3a47f631dafb35
Nishant <nish.kumbhar02@gmail.com>
2021-12-07 18:46:14 +0530

    Add files via upload

---
+++ assertion.py
@@ -0,0 +1,8 @@
+def main():
+    numerator = int(input("Enter the Numerator : "))
+    denominator = int(input("Enter the Denominator : "))
+    assert denominator != 0, "Cannot Divide by zero."
+    print("YZ")
+
+
+main()
