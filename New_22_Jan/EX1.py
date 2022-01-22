e0e246dc5888268154d4416c46bbe609f8a05d88
Nishant <nish.kumbhar02@gmail.com>
2022-01-22 19:32:34 +0530

    Add files via upload

---
+++ New_22_Jan/EX1.py
@@ -0,0 +1,28 @@
+"""
+@author : Nishant Sanjay Kumbhar
+@goal : user input 1 -> 26
+        user input 2 -> 6
+
+            then ->   26          print  ->-> 26
+                      26*26                 -> 676
+                      26*26*26               -> 17576
+                      .
+                      .
+                      .
+                      26*26*26*26*26*26         -> .....
+"""
+import sys
+
+
+def main():
+    num = int(input("Enter the Number i.e model -> "))
+    it = int(input("Enter the count till iterate : "))
+    flag = num
+    for i in range(it):
+        ans = flag
+        for j in range(i):
+            ans *= num
+        print(ans)
+
+
+main()
